588ace98f29dccd68e4f5d09c919a63c7a663692
Norbert <norbert.scehura@microsoft.wsei.edu.pl>
2019-02-24 11:04:01 +0100

    edit notes

--- note-pocket/main.js
+++ note-pocket/main.js
@@ -5,21 +5,37 @@ const noteContent = document.querySelector('#note-content');
 const noteAdd = document.querySelector('#note-add');
 const noteColors = document.querySelector('#note-colors');
 const noteColor = document.querySelector('#note-color');
+const addNewNote = document.querySelector('#note-add-new');
 
 const NOTES_STORE = 'notesStore';
 
 const COLORS = [
   '#FFA500',
   '#FFEB3B',
-  '#F44336'
+  '#F44336',
+  '#CDDC39',
+  '#2196F3',
+  '#9C27B0'
 ];
 
+let isEditing = false;
+let currentEditNoteIndex = -1;
+
 noteAdd.addEventListener('click', () => {
-  const note = new Note({ 
+  if (noteTitle.value === '' || noteContent === '') {
+    return;
+  }
+  
+  if (isEditing) {
+    return saveEditNote();
+  }
+
+  const note = new Note({
     title: noteTitle.value,
     content: noteContent.value,
     color: noteColor.value
   });
+
 
   Notes.saveNote(note);
 
@@ -27,9 +43,17 @@ noteAdd.addEventListener('click', () => {
   noteContent.value = null;
   noteColor.value = null;
 
-  [...noteColors.children].forEach((noteColor) => {
-    noteColor.classList.remove('color');
-  });
+  disableColorButton();
+});
+
+addNewNote.addEventListener('click', () => {
+  isEditing = false;
+
+  noteTitle.value = null;
+  noteContent.value = null;
+  noteColor.value = null;
+
+  noteAdd.innerHTML = 'Dodaj';
 });
 
 class Note {
@@ -53,6 +77,25 @@ class Notes {
   static getNotes() {
     return JSON.parse(localStorage.getItem(NOTES_STORE)) || [];
   }
+
+  static editNote(index) {
+    const notes = Notes.getNotes();
+
+    return notes[index];
+  }
+
+  static saveEditNote(index, note) {
+    const notes = Notes.getNotes();
+
+    notes[index] = {
+      title: note.title,
+      content: note.content,
+      color: note.color
+    }
+
+    localStorage.setItem(NOTES_STORE, JSON.stringify(notes));
+    renderNotes();
+  }
 }
 
 
@@ -63,33 +106,35 @@ function renderNotes() {
     return noNotes.style.display = 'block';
   }
   noteList.innerHTML = '';
-  notes.forEach(note => {
-    const li = document.createElement('li');
-    const title = document.createElement('span');
-    const content = document.createElement('span');
-    const pinned = document.createElement('span');
-    const remove = document.createElement('span');
+  notes
+    .sort(note => note.pinned ? -1 : 1)
+    .forEach((note, index) => {
+      const li = document.createElement('li');
+      const title = document.createElement('span');
+      const content = document.createElement('span');
+      const pinned = document.createElement('span');
+      const remove = document.createElement('span');
 
-    title.classList.add('note-title');
-    content.classList.add('note-content');
-    pinned.classList.add('note-pinned');
-    remove.classList.add('note-remove');
+      title.classList.add('note-title');
+      content.classList.add('note-content');
+      pinned.classList.add('note-pinned');
+      remove.classList.add('note-remove');
 
-    title.innerText = note.title;
-    content.innerText = note.content || null;
+      title.innerText = note.title;
+      content.innerText = note.content || null;
 
-    li.style.borderLeft = `2px solid ${note.color}`;
+      li.style.borderLeft = `4px solid ${note.color}`;
 
-    li.appendChild(title);
-    li.appendChild(remove);
-    li.appendChild(content);
+      li.dataset.index = index;
 
-    li.addEventListener('click', () => {
-      console.log('note clicked!');
+      li.appendChild(title);
+      li.appendChild(remove);
+      li.appendChild(content);
+
+      li.addEventListener('click', editNote);
+
+      noteList.appendChild(li);
     });
-
-    noteList.appendChild(li);
-  });
 
   noNotes.style.display = 'none';
 }
@@ -100,18 +145,55 @@ function renderColors() {
 
     colorButton.classList.add('note-color-button');
     colorButton.style.background = color;
+    colorButton.dataset.color = color;
 
     colorButton.addEventListener('click', (event) => {
       noteColor.value = color;
 
-      [...noteColors.children].forEach((noteColor) => {
-        noteColor.classList.remove('color');
-      });
+      disableColorButton();
 
       event.target.classList.add('color');
     });
 
     noteColors.appendChild(colorButton);
+  });
+}
+
+function editNote() {
+  const index = this.dataset.index;
+
+  isEditing = true;
+  currentEditNoteIndex = index;
+  const note = Notes.editNote(index);
+
+  noteAdd.innerHTML = 'Zapisz';
+
+  noteTitle.value = note.title;
+  noteContent.value = note.content;
+  noteColor.value = note.color;
+
+  disableColorButton();
+
+  [...noteColors.children].forEach((noteColor) => {
+    if (noteColor.dataset.color === note.color) {
+      noteColor.classList.add('color');
+    }
+  });
+}
+
+function saveEditNote() {
+  const note = {
+    title: noteTitle.value,
+    content: noteContent.value,
+    color: noteColor.value
+  }
+
+  Notes.saveEditNote(currentEditNoteIndex, note);
+}
+
+function disableColorButton() {
+  [...noteColors.children].forEach((noteColor) => {
+    noteColor.classList.remove('color');
   });
 }
 
